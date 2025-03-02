5b0f3098ec131c971abef9fe1377575d8d11c8ea
Tahzeeb-sh0 <taizibshaikh34@gmail.com>
2025-03-02 22:50:14 +0530

    Scroll Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,8 +7,15 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './components/Home'
 import { Outlet } from 'react-router-dom'
+import LocomotiveScroll from 'locomotive-scroll';
+
 
 function App() {
+  useEffect(() => { 
+    const locomotiveScroll = new LocomotiveScroll();
+    
+
+  }, [])
   return (
     <>
    <Header />
