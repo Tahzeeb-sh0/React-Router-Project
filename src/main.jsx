2166c5a0d9acec4450da783e22132100e202dedf
Tahzeeb shaikh <taizibshaikh34@gmail.com>
2025-10-22 10:48:38 +0530

    Wrap RouterProvider in React.StrictMode

--- src/main.jsx
+++ src/main.jsx
@@ -37,9 +37,9 @@ const router = createBrowserRouter(
 
 createRoot(document.getElementById('root')).render(
 
-   
+     <React.StrictMode>
 <RouterProvider router={router}/>
-
+  </React.StrictMode>
  
 
 )
